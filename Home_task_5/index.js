dfcc1cc985f5c99bd1050cc91fafba7e7d9641e8
OlhaKovalova75 <olgaterralayn@ukr.net>
2022-08-16 11:53:53 +0300

    unit home tasks in main folder

---
+++ Home_task_5/index.js
@@ -0,0 +1,31 @@
+let numberOne = +prompt('Enter number one, please');
+let symbol = prompt('Choose simbol +-*/');
+let numberTwo = +prompt('Enter number two, please');
+let noteOne = 'Your result is RESULT';
+let sum = +(numberOne + numberTwo);
+let minus = +(numberOne - numberTwo);
+let asterisk = +(numberOne * numberTwo);
+let division = +(numberOne / numberTwo);
+
+if (symbol === '+') {
+    alert(sum);
+    console.log(sum);
+}
+else if (symbol === '-') {
+    alert(minus);
+    console.log(minus)
+}
+
+else if (symbol === '*') {
+    alert(asterisk);
+    console.log(asterisk)
+}
+else if (symbol === '/') {
+    alert(division);
+    console.log(division)
+}
+else {
+    if (symbol != sum && minus && asterisk && division) {
+        (alert('This operation is not exist'))
+    }
+}
